cf909056f9f6894207bdc984771202a8bfb0dbaa
vermavedu47-maker <vermavedu47@gmail.com>
2026-01-15 19:34:38 +0530

    Create signup.js

---
+++ pages/signup.js
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Create your SubHub account</h2>
+      <p>Signup functionality will be connected next.</p>
+    </div>
+  );
+}
