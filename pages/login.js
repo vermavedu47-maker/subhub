0f474964853e3190c370a0a0890b98fcf0d7a716
vermavedu47-maker <vermavedu47@gmail.com>
2026-01-15 20:13:05 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,5 @@
 import { initDB } from "../lib/initSupabase";
-initDB(); // this will create the table automatically
+ // this will create the table automatically
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
