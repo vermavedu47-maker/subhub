b7e5440b0c58df65818696dd60f5ef2c1ed6c422
vermavedu47-maker <vermavedu47@gmail.com>
2026-01-15 19:01:14 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,21 @@
+import { signIn } from "next-auth/react";
+
 export default function Login() {
   return (
     <div style={{ padding: 40 }}>
       <h2>Login to SubHub</h2>
-      <p>Authentication setup coming next.</p>
+      <button
+        onClick={() =>
+          signIn("credentials", {
+            email: "admin@subhub.in",
+            password: "admin123",
+            callbackUrl: "/"
+          })
+        }
+        style={{ padding: 10, marginTop: 20 }}
+      >
+        Login as Admin
+      </button>
     </div>
   );
 }
