9542d9223b7d466f2f92cbad5cda9776e6cc965c
vermavedu47-maker <vermavedu47@gmail.com>
2026-01-15 18:52:25 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>🚀 SubHub</h1>
+      <p>India’s creator subscription platform</p>
+      <p>Buy & sell digital products, communities and subscriptions.</p>
+    </div>
+  );
+}
