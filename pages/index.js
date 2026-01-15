1d00c775efa18d83d605d643de770b072d60f65f
vermavedu47-maker <vermavedu47@gmail.com>
2026-01-15 19:34:01 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div style={{ padding: 40, fontFamily: "sans-serif" }}>
       <h1>🚀 SubHub</h1>
       <p>India’s creator subscription platform</p>
-      <p>Buy & sell digital products, communities and subscriptions.</p>
+
+      <div style={{ marginTop: 20 }}>
+        <Link href="/login">
+          <button style={{ marginRight: 10 }}>Login</button>
+        </Link>
+
+        <Link href="/signup">
+          <button>Sign Up</button>
+        </Link>
+      </div>
+
+      <p style={{ marginTop: 30 }}>
+        Buy & sell digital products, communities and subscriptions.
+      </p>
     </div>
   );
 }
